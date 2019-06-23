b0ad2e44431c5aaf1e962731256a2bb6c9aafeaa
moises <moigomes@gmail.com>
2019-06-23 03:03:45 -0300

    Refatoracao e simplificação

--- core/transform.py
+++ core/transform.py
@@ -1,42 +1,30 @@
-import requests
+import logging
+
+import geocoder
 
 
 class Transform:
 
-    def __init__(self, key_google_maps,
-                 url_base='https://maps.googleapis.com/maps/api/geocode/json?'):
-
-        self.__url_base = url_base
+    def __init__(self, key_google_maps: str):
         self.__key_google_maps = key_google_maps
 
-        self.__address_components_type = {'rua': 'route',
-                                          'numero': 'street_number',
-                                          'bairro': 'sublocality',
-                                          'cidade': 'administrative_area_level_2',
-                                          'cep': 'postal_code',
-                                          'uf': 'administrative_area_level_1',
-                                          'pais': 'country'}
+    def get_endereco(self, latitude: float, longitude: float) -> dict:
+        try:
+            dados_retornados: geocoder = geocoder.google([latitude, longitude],
+                                                         method='reverse',
+                                                         key=self.__key_google_maps,
+                                                         language='pt')
 
-    def get_adress(self, pair_of_coordinates):
-        response = requests.get(self.__get_ful_url(pair_of_coordinates)).json()
-        return self.__mount_dict_with_address(pair_of_coordinates, response)
+            return {'latitude': latitude,
+                    'longitude': longitude,
+                    'rua': dados_retornados.street_long,
+                    'numero': dados_retornados.housenumber,
+                    'bairro': dados_retornados.sublocality,
+                    'cidade': dados_retornados.county,
+                    'cep': dados_retornados.postal,
+                    'uf': dados_retornados.state,
+                    'pais': dados_retornados.country_long}
 
-    def __get_ful_url(self, pair_of_coordinates):
-        return f'{self.__url_base}latlng={pair_of_coordinates[0]},{pair_of_coordinates[1]}&key={self.__key_google_maps}'
-
-    @staticmethod
-    def __get_response_address_components(response):
-        return response['results'][0]['address_components']
-
-
-    def __mount_dict_with_address(self, pair_of_coordinates, response):
-        adress_dict = {'latitude': pair_of_coordinates[0], 'longitude': pair_of_coordinates[1]}
-        for component_type in self.__address_components_type.items():
-            adress_dict[component_type[0]] = ''
-            for component in self.__get_response_address_components(response):
-                if component_type[1] in component['types']:
-                    adress_dict[component_type[0]] = component['short_name']
-                    break
-
-        return adress_dict
-
+        except Exception as erro:
+            logging.error(f'Erro ao realizar requisição usando geocoder.google! - {erro}')
+            exit()
